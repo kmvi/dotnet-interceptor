d7de8743b8b7f6d302c82bab4040ebe9680d0115
kmvi <kmvi@users.noreply.github.com>
2016-09-14 11:05:45 +0300

    fix emitter

--- NETInterceptor/Emitter.cs
+++ NETInterceptor/Emitter.cs
@@ -11,39 +11,6 @@ namespace NETInterceptor
         private static readonly object _sync = new object();
         private static readonly Lazy<ModuleBuilder> _builder = new Lazy<ModuleBuilder>(CreateBuilder);
 
-        public static Type EmitDelegate(Type[] args)
-        {
-            return EmitDelegate(args, typeof(void));
-        }
-
-        public static Type EmitDelegate(Type[] args, Type ret)
-        {
-            if (args == null)
-                args = new Type[0];
-
-            lock (_sync) {
-                return Emit(args, ret);
-            }
-        }
-
-        private static Type Emit(Type[] args, Type ret)
-        {
-            var tb = _builder.Value.DefineType("__" + Guid.NewGuid().ToString("N"),
-                            TypeAttributes.Class | TypeAttributes.Public | TypeAttributes.Sealed | TypeAttributes.AnsiClass | TypeAttributes.AutoClass,
-                            typeof(MulticastDelegate));
-
-            var cb = tb.DefineConstructor(MethodAttributes.RTSpecialName | MethodAttributes.HideBySig | MethodAttributes.Public,
-                CallingConventions.Standard, new Type[] { typeof(object), typeof(IntPtr) });
-
-            cb.SetImplementationFlags(MethodImplAttributes.Runtime | MethodImplAttributes.Managed);
-
-            var mb = tb.DefineMethod("Invoke", MethodAttributes.Public | MethodAttributes.HideBySig | MethodAttributes.NewSlot | MethodAttributes.Virtual, ret, args);
-
-            mb.SetImplementationFlags(MethodImplAttributes.Runtime | MethodImplAttributes.Managed);
-
-            return tb.CreateType();
-        }
-
         public static MethodInfo EmitMethod(MethodInfo info)
         {
             lock (_sync) {
@@ -79,11 +46,13 @@ namespace NETInterceptor
             for (int i = 0; i < 20; ++i)
                 gen.Emit(OpCodes.Nop);
 
-            // TODO: return ref value case
-            var loc = gen.DeclareLocal(info.ReturnType);
-            gen.Emit(OpCodes.Ldloc, loc);
-            gen.Emit(OpCodes.Initobj, info.ReturnType);
-            gen.Emit(OpCodes.Ldloc, loc);
+            if (info.ReturnType != typeof(void)) {
+                // TODO: return ref value case
+                var loc = gen.DeclareLocal(info.ReturnType);
+                gen.Emit(OpCodes.Ldloc, loc);
+                gen.Emit(OpCodes.Initobj, info.ReturnType);
+                gen.Emit(OpCodes.Ldloc, loc);
+            }
             gen.Emit(OpCodes.Ret);
 
             return tb.CreateType().GetMethod(methodName);
